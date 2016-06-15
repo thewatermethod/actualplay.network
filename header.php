591d518b4a38135df91453ccbf4121fde517a083
Matt Bevilacqua <mbevilac@providence.edu>
2016-06-15 14:52:30 -0400

    header/nav styles for interior pages

--- header.php
+++ header.php
@@ -25,35 +25,37 @@
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'actual-play' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( get_header_image() ) : ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-				</a>
-			<?php endif; // End header image check. ?>
+		<?php if ( !is_front_page() && !is_home() ) :  echo'<div class="branding-wrapper">';  endif;?>
+			<div class="site-branding">
+				<?php if ( get_header_image() ) : ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+					</a>
+				<?php endif; // End header image check. ?>
 
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
 
+				?>
+			</div><!-- .site-branding -->
+
+			<?php if ( !is_front_page() && !is_home() ) :  echo'<i class="fa fa-bars" aria-hidden="true"></i>';  endif;?>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+
+			<?php 
+				if ( is_front_page() && is_home() ) : 
+					dynamic_sidebar( 'home-sidebar' ); 
+				endif;
 			?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'actual-play' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-
-		<?php 
-			if ( is_front_page() && is_home() ) : 
-				dynamic_sidebar( 'home-sidebar' ); 
-			endif;
-		?>
-
+		<?php if ( !is_front_page() && !is_home() ) :  echo'</div>';  endif;?>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
