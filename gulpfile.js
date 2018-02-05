552efe8d5db6883a7550f3a6a553b339a764f304
Matt Bevilacqua <mbevilac@providence.edu>
2018-02-05 08:42:03 -0500

    some inital reductions, removed node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 	Open command prompt as administrator
 
 	cd YOUR-THEME-DIRECTORY
-	npm install gulp gulp-concat gulp-uglify gulp-sourcemaps gulp-less gulp-clean-css run-sequence gulp-watch browser-sync --save-dev 
+	npm install gulp gulp-concat gulp-uglify gulp-sourcemaps gulp-less gulp-clean gulp-clean-css run-sequence  --save-dev 
 	gulp
 
 */
@@ -15,9 +15,9 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	less = require('gulp-less'),
 	cleanCSS = require('gulp-clean-css'),
-	runSequence = require('run-sequence'),
-	watch = require('gulp-watch'),
-	browserSync = require('browser-sync').create();
+	runSequence = require('run-sequence');
+
+	
 
 //this task builds the less files into minified css
 gulp.task('build-less', function(){
@@ -28,17 +28,8 @@ gulp.task('build-less', function(){
 		.pipe( cleanCSS() )
 		.pipe( sourcemaps.write('maps') )
 		.pipe( gulp.dest('min') )
-		.pipe( browserSync.stream() );
 });
 
-
-gulp.task('watch', function() {
-	gulp.watch('less/*.less', ['compile']);
-	gulp.watch('*.php').on('change', browserSync.reload);
-	gulp.watch('inc/*.php').on('change', browserSync.reload);
-	gulp.watch('template-parts/*.php').on('change', browserSync.reload);
-	gulp.watch(['js/*.js', '*.js']).on('change', browserSync.reload);
-});
 
 
 gulp.task('compile', function(){
@@ -47,20 +38,12 @@ gulp.task('compile', function(){
 	);
 });
 
-gulp.task('serve', function(){
-	// browserSync.init({
-	// 	//change name of proxy to local dev URL
-	// 	proxy: 'localhost/actualplay.network',
-	// 	port: '81'
-	// });
-});
+
 
 
 gulp.task( 'default', function(){
 	runSequence(
-		//'watch',
 		'compile'
-		//'serve'
 	);
 
 });
