df7cfe330df4f37f6ceae4587c35c911042b8bf6
Matt Bevilacqua <mbevilac@providence.edu>
2021-02-05 13:13:03 -0500

    add post meta to rest response

--- functions.php
+++ functions.php
@@ -185,3 +185,22 @@ add_filter('body_class','apn_add_category_name');
  */
 require get_template_directory() . '/inc/css.php';
 
+add_action( 'rest_api_init', 'create_api_posts_meta_field' );
+
+function create_api_posts_meta_field() {
+
+	register_rest_field( 'post', 'post-meta', 
+		array(
+			'get_callback' => 'get_post_meta_for_api',
+			'schema' => null,
+		)
+ 	);
+}
+
+function get_post_meta_for_api( $object ) {
+	//get the id of the post object array
+	$post_id = $object['id'];
+
+	//return the post meta
+	return get_post_meta( $post_id );
+}
